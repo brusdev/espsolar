07435550782aa7f3f8747a976683bde7068ec646
BrusDev <brusdev@gmail.com>
2017-04-26 02:17:50 +0200

    It runs.

--- test.lua
+++ test.lua
@@ -17,30 +17,20 @@ wifi_gateway="192.168.50.1"
 thingspeak_channel_id="232899"
 thingspeak_write_api_key="NHOKPWL6M9X4993X"
 
---[[Debug settings
 sleep_conversion=1*1000
-sleep_duration=5*1000
+sleep_duration=15*1000
+sync_timeout=60*1000
 run_timeout=30*1000
-sent_timeout=60*1000
 sntp_timeout=24*60*60*1000
---]]
----[[Run settings
-sleep_conversion=1*1000
-sleep_duration=5*60*1000
-run_timeout=10*1000
-sent_timeout=1*60*60*1000
-sntp_timeout=24*60*60*1000
---]]
 
 rtc_time=0
-rtc_begin=14
-rtc_length=114
+rtc_begin=8
+rtc_length=120
 
-queue_state=0
-queue_head=0
-queue_count=0
-queue_sent=0
-sent_time=0
+rtc_state=0
+rtc_index=0
+rtc_count=0
+record_time=0
 sntp_time=0
 
 ds18b20 = require("ds18b20")
@@ -49,32 +39,30 @@ function boot()
   print("boot.begin")
   
   rtc_time=rtctime.get()
-  print("boot.rtc_time:"..rtc_time)
   
-  if (rtc_time~=0) then
-    local queue_data=rtcmem.read32(10)
-    queue_state=bit.band(queue_data/0x1, 0xFF)
-    print("boot.queue_state:"..queue_state)
-    queue_head=bit.band(queue_data/0x100, 0xFF)
-    print("boot.queue_head:"..queue_head)
-    queue_count=bit.band(queue_data/0x10000, 0xFF)
-    print("boot.queue_count:"..queue_count)
-    queue_sent=queue_count
-    print("boot.queue_sent:"..queue_sent)
-    sent_time=rtcmem.read32(11)
-    print("boot.sent_time:"..sent_time)
-    sntp_time=rtcmem.read32(12)
+  --Check rtc memory.
+  local rtc_init=rtcmem.read32(0)
+  if (rtc_init==0x44695553) then
+    rtc_state=rtcmem.read32(1)
+    print("boot.rtc_state:"..rtc_state)
+    rtc_index=rtcmem.read32(2)
+    print("boot.rtc_index:"..rtc_index)
+    rtc_count=rtcmem.read32(3)
+    print("boot.rtc_count:"..rtc_count)
+    record_time=rtcmem.read32(3)
+    print("boot.record_time:"..record_time)
+    sntp_time=rtcmem.read32(4)
     print("boot.sntp_time:"..sntp_time)
   end
   
-  if queue_state==1 and rtc_time~=0 and rtc_time-sent_time<sent_timeout/1000 then
+  if rtc_state==1 and rtc_time~=0 and rtc_time-record_time<sync_timeout/1000 then
     ds18b20.convert(1)
     ds18b20.convert(2)
-    queue_state=2
+    rtc_state=2
     sleep(sleep_conversion, "end")
-  elseif queue_state==2 then
+  elseif rtc_state==2 then
     push_record()
-    queue_state=1
+    rtc_state=2
     sleep(sleep_duration, "end")
   else
     ds18b20.convert(1)
@@ -92,34 +80,33 @@ function boot()
 end
 
 function push_record()
-  local head=queue_head+rtc_begin
+  local head=rtc_index+rtc_begin
   print("push_record.head: "..head)
-  local timestamp=rtc_time
+  local timestamp=rtctime.get()
+  print("push_record.timestamp: "..timestamp)
   local temperatures=ds18b20.read(1)*0x10000+ds18b20.read(2)
+  print("push_record.temperatures: "..temperatures)
   local diagnostics=adc.readvdd33()*0x10000+tmr.now()/1000
-  
+  print("push_record.diagnostics: "..diagnostics)
+  record_time=rtctime.get()
   rtcmem.write32(head, timestamp)
   rtcmem.write32(head+1, temperatures)
   rtcmem.write32(head+2, diagnostics)
   
-  queue_head=(queue_head+3)%rtc_length
-  queue_count=queue_count+3
-  if (queue_count>rtc_length) then
-    queue_count=rtc_length
-  end
-  queue_sent=queue_sent+3
-  if (queue_sent>rtc_length) then
-    queue_sent=rtc_length
-  end
+  rtc_index=(rtc_index+3)%rtc_length
+  rtc_count=rtc_count+3
 end
 
-function pop_record()
-  if queue_sent>0 then
-    local tail=((queue_head-queue_sent)%rtc_length)+rtc_begin
+function peak_record()
+  if rtc_count>0 then
+    local tail=((rtc_index-rtc_count)%rtc_length)+rtc_begin
     print("peak_record.tail: "..tail)
     local timestamp=rtcmem.read32(tail)
+    print("peak_record.timestamp: "..timestamp)
     local temperatures=rtcmem.read32(tail+1)
+    print("peak_record.temperatures: "..temperatures)
     local diagnostics=rtcmem.read32(tail+2)
+    print("peak_record.diagnostics: "..diagnostics)
     local etemp=ds18b20.decode(temperatures/0x10000);
     local itemp=ds18b20.decode(bit.band(temperatures, 0xFFFF));
     local record=
@@ -128,12 +115,18 @@ function pop_record()
     ",\"field2\":"..(itemp/10000).."."..(itemp%10000)..
     ",\"field3\":"..diagnostics/0x10000..
     ",\"field4\":"..bit.band(diagnostics, 0xFFFF).. "}"
-    queue_sent=queue_sent-3
+    print("peak_record: " .. record)
+    rtc_time=timestamp
     return record..string.rep(" ", 100 - string.len(record))
   else
     return nil
   end
 end
+
+function pop_record()
+  rtc_count=rtc_count-3
+end
+
 
 function on_wifi_got_ip()
   print("on_wifi_got_ip.begin")
@@ -148,8 +141,6 @@ end
 
 function on_sntp_sync_success()
   print("on_sntp_sync_success.begin")
-  rtc_time=rtctime.get()
-  print("on_sntp_sync_success.rtc_time:"..rtc_time)
   thingspeak_sync(function() sleep(sleep_duration, "end") end)
 end
 
@@ -164,54 +155,53 @@ end
 
 function thingspeak_sync(callback)
   print("thingspeak_sync.begin")
+  local record_pushed=false
   local thingspeak_socket_connected=false
-  local thingspeak_socket_sending=false
   local thingspeak_socket=net.createConnection(net.TCP, 0)
     
   local function on_connection(socket)
     print("on_connection.begin")
     thingspeak_socket_connected=true
-    thingspeak_socket_sending=true
     
     local content = "{\"write_api_key\":\"" .. thingspeak_write_api_key .. "\", \"updates\":["
-    local content_length=string.len(content) + (queue_count / 3) * 101 + 102
+    local content_length=string.len(content) + (rtc_count / 3) * 100 + 2
     local data = "POST /channels/".. thingspeak_channel_id .. "/bulk_update.json HTTP/1.1\r\n" ..
       "HOST: api.thingspeak.com\r\n" ..
       "Content-Length: " ..content_length.. "\r\n" ..
       "Content-Type: application/json\r\n\r\n" .. content
         
-    --print("on_connection.send: " .. data)
+    print("on_connection.send: " .. data)
     socket:send(data)
   end
 
   local function on_sent(socket)
-    if (thingspeak_socket_sending) then
-      print("on_sent.begin")
-      local data=pop_record()
-      
-      if data==nil then
-        push_record()
-        data=pop_record()
-        data=data.."]}"
-        thingspeak_socket_sending=false
-      else
-        data=data..","
-      end
-      
+    print("on_sent.begin")
+    local data=nil
+    print("on_sent.rtc_count: "..rtc_count)
+    
+    if rtc_count==0 and record_pushed==false then
+      push_record()
+      record_pushed=true
+    end
+    
+    if rtc_count > 0 then
+      data=peak_record()
+      pop_record()
       print("on_sent.send: " .. data)
       socket:send(data)
+      if rtc_count==0 then
+        data="]}"
+        print("on_sent.send: " .. data)
+        socket:send(data)
+      end
     end
   end
   
   local function on_receive(socket, data)
     print("on_receive.begin")
-    --print("on_receive.data: " .. data)
+    print("on_receive.data: " .. data)
     if (string.find(data, "Status: 202 Accepted") ~= nil) then
       print("on_receive.success")
-      queue_state=1
-      queue_count=0
-      queue_head=0
-      sent_time=rtc_time
       sleep(sleep_duration, "run_timeout")
     end
     print("on_receive.socket::close(--)")
@@ -229,7 +219,7 @@ function thingspeak_sync(callback)
   thingspeak_socket:on("receive", on_receive)
   thingspeak_socket:on("sent", on_sent)
   
-  print("thingspeak_sync.queue_count: "..queue_count)
+  print("thingspeak_sync.rtc_count: "..rtc_count)
   
   thingspeak_socket:connect(80, "api.thingspeak.com")
 end
@@ -237,10 +227,12 @@ end
 function sleep(duration, reason)
     print("sleep.begin")
     
-    local queue_data=queue_state*0x1+queue_head*0x100+queue_count*0x10000
-    rtcmem.write32(10, queue_data)
-    rtcmem.write32(11, sent_time)
-    rtcmem.write32(12, sntp_time)
+    rtcmem.write32(0, 0x44695553)
+    rtcmem.write32(1, rtc_state)
+    rtcmem.write32(2, rtc_index)
+    rtcmem.write32(3, rtc_count)
+    rtcmem.write32(4, record_time)
+    rtcmem.write32(5, sntp_time)
   
     print("sleep.duration: " .. duration)
     print("sleep.reason: " .. reason)
@@ -250,8 +242,8 @@ function sleep(duration, reason)
 end
 
 --Verifico se stato disattivata l'esecuzione.
-gpio.mode(5, gpio.INPUT, gpio.PULLUP)
-if gpio.read(5) ~= 0 then
+gpio.mode(3, gpio.INPUT, gpio.PULLUP)
+if gpio.read(3) ~= 0 then
   boot()
   tmr.alarm(0, run_timeout, tmr.ALARM_SINGLE, function() sleep(sleep_duration, "run_timeout") end)
 end
